5c1220d0d84314b99855a63653a3c5a00587c962
Este <anthony.garces1995@gmail.com>
2019-10-28 19:59:53 -0500

    Added questions array in a separate js file and made vars for the elements that I'm going to manipulate later

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        Choices : ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within _______ ",
+        Choices : ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The arrays in javascript can be used to store  ",
+        Choices : ["strings", "numbers", "arrays", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "What is the powerful tool provided in Google Chrome that you can use to code any errors in your code",
+        Choices : ["window", "console", "Chrome Developer Tools", "javascript"],
+        answer: "Chrome Developer Tools"
+    },
+    {
+        title: "What command can you use to end the function upon use",
+        Choices : ["stop", "end", "return", "finish"],
+        answer: "return"
+    }
+]
--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-questionsEL = document.getElementById("");
+var startQuizEl = document.getElementsByClassName("StartQuiz");
+var questionBoxEl = document.getElementsByClassName("Questionbox");
+var endOfQuizEl = document.getElementsByClassName("EndofQuiz");
+var highScoresEl = document.getElementsByClassName("StartQuiz");
+var buttOne = document.getElementsByClassName("Answer1");
+var buttTwo = document.getElementsByClassName("Answer2");
+var buttThree = document.getElementsByClassName("Answer3");
+var buttFour = document.getElementsByClassName("Answer4");
+var answerEl = document.getElementsByClassName("RightorWrong");
+var answerSubmitEl = document.getElementsByClassName("Inisubmit");
+var scoreListEl = document.getElementsByClassName("NameList");
+var startButt = document.getElementsByClassName("StartButt");
+
 //do this for each element in the html to make it easier to reference in the javascript
 
 // blank timer id
