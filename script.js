04b659b7d499268c3408f3ae6833a08cfa0a0a10
Este <anthony.garces1995@gmail.com>
2019-11-01 21:46:31 -0500

    Updating with non-working code that reads the contents of the selected button, along with some notes detailing steps

--- script.js
+++ script.js
@@ -21,9 +21,44 @@ var questBox2El = document.getElementsByClassName("questBox2");
 var questBox3El = document.getElementsByClassName("questBox3");
 var questBox4El = document.getElementsByClassName("questBox4");
 
+function startQuiz() {
+    startQuizEl[0].setAttribute("style", "display: none");
+    questionBoxEl[0].setAttribute("style", 'display: block'); 
+};
 
+function changeQuestion() {
+    for (var i = 0; i < questions.length; i++) {
+        spaceBoxEl[0].innerText = questions[i].title;
+        questBox1El[0].innerHTML = questions[i].Choices[0];
+        questBox2El[0].innerHTML = questions[i].Choices[1];
+        questBox3El[0].innerHTML = questions[i].Choices[2];
+        questBox4El[0].innerHTML = questions[i].Choices[3];
+        
+        var rightAnswer = questions[i].answer;
+        //need to identify the clicked button, and check the html of that button against the right answer from questions.js
+        function returnID(clicked_ID) {
+            return clicked_ID;
+        };
+        var clickedButtonID = returnID(clicked_ID);
+        
+        //if the button html is the right answer, display Right! else display Wrong!
+        if (clickedButtonID.innerHTML === rightAnswer) {
+            RightEl[0].setAttribute("style", "display: block");
+        } else {
+            WrongEl[0].setAttribute("style", "display: block");
+            seconds - 5;
+        }
+        //when a button is pressed, add 1 to i and iterate it all over again
+        questionButton.addEventListener('click', event => {
+            i++;
+        });
+
+    }
+}
+    
 //do this for each element in the html to make it easier to reference in the javascript
-function startTimer() {
+function startGame() {
+    //starts the timer //working
     var interval = setInterval(function() {
         seconds--;
         console.log(seconds);
@@ -33,27 +68,18 @@ function startTimer() {
             clearInterval(interval);
         }
     }, 1000);
-
-}
-// change from content div 1 to content div 2
-function startQuiz() {
-    startQuizEl[0].setAttribute("style", "display: none");
-    questionBoxEl[0].setAttribute("style", 'display: block');
+    
+    //change the startQuizEl to questBoxEl //working
+    startQuiz();
+    
+    //Add the questions and buttons to questBoxEl from questions.js
+    //checks the clicked button against the answer from questions.js //not working
+    //display the answer 
+    changeQuestion();
     
 }
 
 
-function changeQuestion() {
-    for (var i = 0; i < questions.length; i++) {
-        spaceBoxEl[0].innerText = questions[0].title;
-        questBox1El[0].innerHTML = questions[0].Choices[0];
-        questBox2El[0].innerHTML = questions[0].Choices[1];
-        questBox3El[0].innerHTML = questions[0].Choices[2];
-        questBox4El[0].innerHTML = questions[0].Choices[3];
-        i++;
-        
-
-startQuiz();
 // changeQuestion();
 
 
